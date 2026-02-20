ef74d4ccc57eb26d78a1ffe7f68fa9053c5fc4f3
Josh Vogt <joshvogt@gmail.com>
2026-02-20 10:40:26 -0500

    Tidying up a few things.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -4,18 +4,25 @@ const domain = new URL(url).hostname;
 module.exports = {
   url,
   domain,
-  siteName: 'ElevindieWeb Starter',
-  siteDescription: 'An Eleventy Starter for IndieWeb',
+  siteName: 'ElevIndieWeb Starter',
+  siteDescription: 'An Eleventy Starter for IndieWeb with support for WebMentions, IndieAuth and MicroPub.',
   lang: 'en',
   author: {
-    name: 'Josh',
+    name: 'Josh Vogt',
     email: 'tearoffapp@proton.me',
     website: url,
     photo: '/assets/images/avatar.jpg',
-    note: 'Josh made this with help from others',
+    note: 'Developer from Toronto.',
     relMe: [
       { name: 'GitHub', url: 'https://github.com/dumaurier' },
-      { name: 'Mastodon', url: 'https://mastodon.social/@tearoff' }
+      { name: 'Mastodon', url: 'https://mastodon.social/@tearoff' },
+      { name: 'BlueSky', url: 'https://bsky.app/profile/tearoff.bsky.social'}
+    ],
+    social: [
+      { name: 'GitHub', url: 'https://github.com/dumaurier' },
+      { name: 'Mastodon', url: 'https://mastodon.social/@tearoff' },
+      { name: 'BlueSky', url: 'https://bsky.app/profile/tearoff.bsky.social'}
+      // Add any other Social Media profiles you have
     ]
   },
   // IndieAuth endpoints — default to indieauth.com, swap if self-hosting
